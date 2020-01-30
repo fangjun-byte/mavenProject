770ce5a20874cbb739e0587707086fa42a19eca8
fangjun <1102735165@qq.com>
2020-01-30 10:50:34 +0800

    my first commit in eclipse

---
+++ src/main/java/com/xdl/git/Apple.java
@@ -0,0 +1,10 @@
+package com.xdl.git;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/mavenProject/AppleTest.java
@@ -0,0 +1,10 @@
+package mavenProject;
+
+public class AppleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
